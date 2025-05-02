727498b5ea766b5a92a7fd6f5e51bf278350ca5e
HhotateA <hhotatea@gmail.com>
2025-05-03 02:30:28 +0900

    v0.0.19

--- Packages/com.hhotatea.avatar-pose-library/Runtime/Component/AvatarPoseLibrary.cs
+++ Packages/com.hhotatea.avatar-pose-library/Runtime/Component/AvatarPoseLibrary.cs
@@ -7,6 +7,7 @@ namespace com.hhotatea.avatar_pose_library.component
     /// <summary>
     /// アバターにアタッチすることでポーズカテゴリを設定可能にするコンポーネント
     /// </summary>
+    [HelpURL("https://github.com/HhotateA/AvatarPoseLibrary/wiki")]
     public class AvatarPoseLibrary : MonoBehaviour, IEditorOnly
     {
         // データの本体。
