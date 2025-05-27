2b2e12f4428692f9028dc6a1a9076ca1f14c3d55
HhotateA <hhotatea@gmail.com>
2025-05-28 07:33:34 +0900

    menu_targetの保持位置変更

--- Packages/com.hhotatea.avatar-pose-library/Editor/Logic/MenuBuilder.cs
+++ Packages/com.hhotatea.avatar-pose-library/Editor/Logic/MenuBuilder.cs
@@ -11,6 +11,12 @@ namespace com.hhotatea.avatar_pose_library.logic
         public static GameObject BuildPoseMenu(AvatarPoseData poseLibrary)
         {
             var result = new GameObject(poseLibrary.name);
+            if (poseLibrary.target != null)
+            {
+                var installer = result.AddComponent<ModularAvatarMenuInstaller>();
+                installer.installTargetMenu = poseLibrary.target;
+            }
+            
             var mResult = result.AddComponent<ModularAvatarMenuItem>();
             mResult.MenuSource = SubmenuSource.Children;
             mResult.Control.icon = poseLibrary.thumbnail;
@@ -47,6 +53,12 @@ namespace com.hhotatea.avatar_pose_library.logic
             {
                 var folder = new GameObject(category.name);
                 folder.transform.SetParent(result.transform);
+                if (category.target != null)
+                {
+                    var installer = folder.AddComponent<ModularAvatarMenuInstaller>();
+                    installer.installTargetMenu = category.target;
+                }
+                
                 var mFolder = folder.AddComponent<ModularAvatarMenuItem>();
                 mFolder.MenuSource = SubmenuSource.Children;
                 mFolder.Control.icon = category.thumbnail;
@@ -57,6 +69,12 @@ namespace com.hhotatea.avatar_pose_library.logic
                 {
                     var item = new GameObject(pose.name);
                     item.transform.SetParent(folder.transform);
+                    if (pose.target != null)
+                    {
+                        var installer = item.AddComponent<ModularAvatarMenuInstaller>();
+                        installer.installTargetMenu = pose.target;
+                    }
+                    
                     var mItem = item.AddComponent<ModularAvatarMenuItem>();
                     mItem.Control.icon = pose.thumbnail;
                     mItem.Control.type = VRCExpressionsMenu.Control.ControlType.Toggle;
