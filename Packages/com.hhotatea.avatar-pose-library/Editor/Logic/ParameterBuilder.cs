d61211ae5d2b23ddd94535d1a6b1c1afeadd09fc
HhotateA <hhotatea@gmail.com>
2025-10-19 03:21:34 +0900

    posespaceの初期値変更

--- Packages/com.hhotatea.avatar-pose-library/Editor/Logic/ParameterBuilder.cs
+++ Packages/com.hhotatea.avatar-pose-library/Editor/Logic/ParameterBuilder.cs
@@ -150,7 +150,7 @@ namespace com.hhotatea.avatar_pose_library.logic
                     nameOrPrefix = $"{ConstVariables.PoseSpaceParamPrefix}_{poseLibrary.Guid}",
                     syncType = ParameterSyncType.Bool,
                     localOnly = true,
-                    defaultValue =  1,
+                    defaultValue =  0,
                     saved = true,
                 });
             }
